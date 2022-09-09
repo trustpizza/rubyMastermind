8f00a551e87038c5ea6a5e7db18c6ad0056b978a
trustpizza <maxelolsson@gmail.com>
2022-09-09 13:56:45 -0500

    Move play function to Player class

--- script.rb
+++ script.rb
@@ -1,12 +1,12 @@
 class Mastermind
-    attr_accessor :game_board
+    attr_accessor :game_board, :computer_guess
     @@game_over = false
     def initialize(p1)
         system('clear')
         @computer_choices = ['red']
         @computer_guess = @computer_choices.sample
         welcome_screen(p1)
-        play(p1, @computer_guess)
+        p1.play(p1, @computer_guess)
     end
 
     def welcome_screen(p1)
@@ -16,16 +16,6 @@ class Mastermind
         puts '====MasterMind:===='
         puts "=======#{p1.name}!======="
         puts '==================='
-    end
-
-    def play(p1, computer_guess)
-        puts "#{p1.name} you have X guesses left over"
-        puts "Guess!"
-        player_guess = gets.chomp.downcase
-        if player_guess == @computer_guess
-            puts 'You win!'
-        else puts 'you lose'
-        end
     end
 end
         
@@ -45,6 +35,18 @@ class Player
     puts 'Hi player, what is your name?'
     @name = gets.chomp
   end
+
+  def play(p1, computer_guess)
+    puts "#{p1.name} you have X guesses left over"
+        puts "Guess!"
+        player_guess = gets.chomp.downcase
+        if player_guess == computer_guess
+            puts '1'
+        elsif computer_guess.any? == player_guess
+            puts '2'
+        else puts 'you lose'
+        end
+    end
 end
 
 p1 = Player.new
